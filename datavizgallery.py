b133009338c7297dc0acd24c1e7a785f3ee654cf
Sam King <samuel.eric.king@gmail.com>
2022-03-28 15:53:17 -0500

    Annotate Outliers

--- datavizgallery.py
+++ datavizgallery.py
@@ -1,9 +1,12 @@
 import os
+import random
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
 import streamlit as st
 from sidebar import render_option, render_nba_player_stats_options
+
+print('imports done')
 
 option = render_option()
 
@@ -20,8 +23,13 @@ if option == 'NBA Player Stats':
     y_axis = render_nba_player_stats_options('Y-Axis', cols, 16)
     size = render_nba_player_stats_options('Size', cols, 0)
 
+    # Define annotations dataframe
+    x_head = df.sort_values(by=[x_axis], ascending=[False]).head(15)
+    y_head = df.sort_values(by=[y_axis], ascending=[False]).head(15)
+    annotations = pd.concat([x_head[['PLAYER', x_axis, y_axis]], y_head[['PLAYER', x_axis, y_axis]]]).drop_duplicates()
+
     # Create Plot
-    fig = plt.figure()
+    fig = plt.figure(figsize=(10,10))
     plt.title(x_axis + ' vs ' + y_axis)
     ax = sns.scatterplot(x_axis,
                          y_axis,
@@ -30,7 +38,11 @@ if option == 'NBA Player Stats':
                          color='#E47041',
                          edgecolor='black'
                          )
-    plt.legend(prop={'size': 7})
+    plt.legend(prop={'size': 9})
+    for i in range(len(annotations)):
+        plt.annotate(annotations['PLAYER'].iloc[i], 
+                     (annotations[x_axis].iloc[i] + (annotations[x_axis].iloc[i] * 0.01), 
+                     (annotations[y_axis].iloc[i] + (annotations[y_axis].iloc[i] * (random.randrange(-2, 2) / 100)))))
     ax.set_facecolor('#c39c76')
     ax.set_axisbelow(True)
     plt.grid(b=True, which="major", linewidth=.5, color='#ffffff')
