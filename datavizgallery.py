c78ef8ebc0a16f88287ce2173792fcda110d1d38
Sam King <samuel.eric.king@gmail.com>
2022-03-28 22:48:53 -0500

    Set up page navigation structure & add Home page

--- datavizgallery.py
+++ datavizgallery.py
@@ -1,10 +1,12 @@
 import os
 import random
+from re import M
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
 import streamlit as st
-from sidebar import render_option, render_nba_player_stats_options
+from home import render_home
+from sidebar import render_option, render_year_selection, optional_variable_checkbox, render_nba_player_stats_options
 
 print('imports done')
 
@@ -12,16 +14,24 @@ option = render_option()
 
 base = os.path.dirname(__file__)
 
-if option == 'NBA Player Stats':
-    df = pd.read_csv(os.path.join(base, 'Data', 'NBA_20_21', 'Player Stats 2020 & 2021.csv'))
+if option == 'Home':
+    render_home()
+elif option == 'NBA':
+    df = pd.read_csv(os.path.join(base, 'Data', 'NBA', 'Player Stats.csv'))
     df.rename(columns={"'+/-": "+/-"})
-
     cols = list(df.columns)
+
+    year = render_year_selection(df)
+    df = df[df['YEAR'] == year]
 
     # Choose Input Variables
     x_axis = render_nba_player_stats_options('X-Axis', cols, 15)
     y_axis = render_nba_player_stats_options('Y-Axis', cols, 16)
-    size = render_nba_player_stats_options('Size', cols, 0)
+    markersize = optional_variable_checkbox('marker size')
+    if markersize:
+        size = render_nba_player_stats_options('Size', cols, 0)
+    else:
+        size = None
 
     # Define annotations dataframe
     x_head = df.sort_values(by=[x_axis], ascending=[False]).head(15)
@@ -29,8 +39,8 @@ if option == 'NBA Player Stats':
     annotations = pd.concat([x_head[['PLAYER', x_axis, y_axis]], y_head[['PLAYER', x_axis, y_axis]]]).drop_duplicates()
 
     # Create Plot
-    fig = plt.figure(figsize=(10,10))
-    plt.title(x_axis + ' vs ' + y_axis)
+    fig = plt.figure(figsize=(9, 9))
+    plt.title(x_axis + ' vs ' + y_axis + ' Over ' + year + ' Season.')
     ax = sns.scatterplot(x_axis,
                          y_axis,
                          data=df,
@@ -40,10 +50,12 @@ if option == 'NBA Player Stats':
                          )
     plt.legend(prop={'size': 9})
     for i in range(len(annotations)):
-        plt.annotate(annotations['PLAYER'].iloc[i], 
-                     (annotations[x_axis].iloc[i] + (annotations[x_axis].iloc[i] * 0.01), 
+        plt.annotate(annotations['PLAYER'].iloc[i],
+                     (annotations[x_axis].iloc[i] + (annotations[x_axis].iloc[i] * 0.01),
                      (annotations[y_axis].iloc[i] + (annotations[y_axis].iloc[i] * (random.randrange(-2, 2) / 100)))))
     ax.set_facecolor('#c39c76')
     ax.set_axisbelow(True)
     plt.grid(b=True, which="major", linewidth=.5, color='#ffffff')
     st.pyplot(fig)
+
+    st.write('Data Source: https://www.nba.com/stats')
--- sidebar.py
+++ sidebar.py
@@ -1,15 +1,23 @@
 import streamlit as st
 
 def render_option():
-    st.sidebar.title("Selection")
+    st.sidebar.title("User Selections:")
     option = st.sidebar.selectbox(
-        "Page Navigation:",
+        'Page Navigation:',
         (
-            "NBA Player Stats",
+            'Home',
+            'NBA',
         ),
     )
     return option
 
+def optional_variable_checkbox(function):
+    return st.sidebar.checkbox(f'Add a {function} variable?')
+
+def render_year_selection(df):
+    year = st.sidebar.selectbox('Choose an NBA Season', ['2021-2022'], 0)
+    return year
+
 def render_nba_player_stats_options(title, cols, default):
-    variable = st.sidebar.selectbox('Choose your ' + title + ' Variable', cols[2:], index=default)
+    variable = st.sidebar.selectbox('Choose your ' + title + ' Variable', cols[4:], index=default)
     return variable
